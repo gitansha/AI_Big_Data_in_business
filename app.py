5d3f203d11a5b98a9d157b493e23f2d692fff9aa
gitansha <43965406+gitansha@users.noreply.github.com>
2024-12-09 06:55:05 +0000

    added the button for sentiment analysis

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def main():
     name = request.form.get("uname")
     return(render_template('main.html'))
 
+@app.route("/SA", methods=['GET','POST'] )
+def sa():
+    return(render_template('SA.html'))
+
+@app.route('/SA_result',methods=['GET','POST'])
+def sa_result():
+    q = request.form.get("q")
+    r = 
+
+    return(render_template('sa_rwsult.html'))
+
+
+
 if __name__ == "__main__":
     app.run(port = 1111)
